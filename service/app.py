0c1464fce700cb49f324a75eef9d53d758fcbe3e
Buck0134 <chenkaiy@andrew.cmu.edu>
2024-04-22 18:40:01 -0700

    Disabled reloader

--- service/app.py
+++ service/app.py
@@ -150,5 +150,5 @@ if __name__ == '__main__':
     # Downlading punkt and stopwords resources. 
     nltk.download('punkt')
     nltk.download('stopwords')
-    app.run(debug=True, host='0.0.0.0', port=5001)
+    app.run(debug=True, host='0.0.0.0', port=5001, use_reloader=False)
 
